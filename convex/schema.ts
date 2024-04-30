681bd33de84428217746892ee8b3a87c049389c3
evanletz <evanletzter@gmail.com>
2024-04-29 23:19:23 -0400

    Remove thumbnails from project

--- convex/schema.ts
+++ convex/schema.ts
@@ -3,25 +3,6 @@ import { v } from "convex/values";
 
 export default defineSchema({
 
-  thumbnails: defineTable({
-    title: v.string(),
-    userId: v.string(),
-    aImage: v.string(),
-    aVotes: v.number(),
-    bImage: v.string(),
-    bVotes: v.number(),
-    voteIds: v.array(v.string()),
-    profileImage: v.optional(v.string()),
-    name: v.optional(v.string()),
-    comments: v.array(v.object({
-      userId: v.string(),
-      text: v.string(),
-      createdAt: v.number(),
-      name: v.string(),
-      profileUrl: v.string(),
-    }))
-  }),
-
   users: defineTable({
     userId: v.string(),
     email: v.string(),
