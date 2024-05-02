fd198557a04bc6a27d1de5caa35dfe3051bb5650
evanletz <evanletzter@gmail.com>
2024-05-02 18:55:39 -0400

    Declare edge runtime for deployment

--- src/app/trades/[tradeId]/page.tsx
+++ src/app/trades/[tradeId]/page.tsx
@@ -6,13 +6,16 @@ import { useQuery } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
 import { ImageEditor } from "@/components/ui/image-editor";
 
+export const runtime = 'edge';
+
 export default function TradesPage() {
   const params = useParams<{ tradeId: Id<"trades"> }>();
   const trade = useQuery(api.trades.getEntry, {
     entryId: params.tradeId,
   });
   if (!trade) {
-    throw new Error('Error: unable to retrieve this entry.')
+    // throw new Error('Error: unable to retrieve this entry.')
+    return <div></div>
   }
   return <ImageEditor image={trade?.imageId} trade={trade} />;
 }
