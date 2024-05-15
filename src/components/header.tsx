36082b28f96f5ce72fa38bc97a0f42ff248a3990
evanletz <evanletzter@gmail.com>
2024-05-15 18:41:06 -0400

    Place menu items inside hamburger on mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,9 +13,9 @@ import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuL
 import { navigationMenuTriggerStyle } from "./ui/navigation-menu";
 import { Button } from "./ui/button";
 import Image from "next/image";
-import { getUser } from "../../convex/util";
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
 
 export function Header() {
   const user = useQuery(api.users.getUser)
@@ -32,7 +32,7 @@ export function Header() {
           </div>
         </div>
         
-
+        <div className="hidden sm:inline">
           <NavigationMenu>
             <SignedIn>
               <NavigationMenuList className="flex gap-8 items-center">
@@ -53,36 +53,70 @@ export function Header() {
               </NavigationMenuList>
             </SignedIn>
           </NavigationMenu>
+        </div>
 
         <div className="w-48 justify-end flex gap-4 items-center">
           <SignedIn>
-            {user && user.profileType === 'free' &&
-              <UpgradeButtonNew />
-            }
-            {user && user.profileType == 'basic' &&
-              <UpgradeButtonExisting price_type="upgrade"/>
-            }
-            <SettingsMenu />
-            <UserButton />
+            <div className="hidden sm:inline">
+              {user && user.profileType === 'free' &&
+                <UpgradeButtonNew />
+              }
+              {user && user.profileType == 'basic' &&
+                <UpgradeButtonExisting price_type="upgrade"/>
+              }
+            </div>
+            <div className="hidden sm:inline"><SettingsMenu /></div>
+            <div className="hidden sm:inline"><UserButton /></div>
           </SignedIn>
           <SignedOut>
-            <NavigationMenu>
-              <SignedOut>
-                <NavigationMenuList className="flex align-right">
-                  <NavigationMenuItem>
-                    <Link href="#pricing" legacyBehavior passHref>
-                      <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                        Pricing
-                      </NavigationMenuLink>
-                    </Link>
-                  </NavigationMenuItem>
-                </NavigationMenuList>
-              </SignedOut>
+            <NavigationMenu className="hidden sm:inline">
+              <NavigationMenuList className="flex align-right">
+                <NavigationMenuItem>
+                  <Link href="#pricing" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                      Pricing
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              </NavigationMenuList>
             </NavigationMenu>
             <SignInButton>
               <Button>Sign In</Button>
             </SignInButton>
           </SignedOut>
+        </div>
+
+
+        <div className="sm:hidden">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="overflow-hidden rounded"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem>Create</DropdownMenuItem>
+              <DropdownMenuItem>Dashboard</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <SettingsMenu />
+                <UserButton />
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                {user && user.profileType === 'free' &&
+                  <UpgradeButtonNew />
+                }
+                {user && user.profileType == 'basic' &&
+                  <UpgradeButtonExisting price_type="upgrade"/>
+                }
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
     </div>
