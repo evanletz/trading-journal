5ce92241451c745daa0190080f4722fad34fd378
evanletz <evanletzter@gmail.com>
2024-05-20 13:31:15 -0400

    Make nextConfig remotePatterns dynamic

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                hostname: 'nautical-malamute-277.convex.cloud',
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        hostname: process.env.CONVEX_HOSTNAME,
+      },
+    ],
+  },
 };
 
 export default nextConfig;
