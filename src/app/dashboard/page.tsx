4793d3f96967fe067da524853aa59dd101f220e2
evanletz <evanletzter@gmail.com>
2024-05-31 01:07:44 -0400

    Adjust card heights at different breakpoints

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -129,7 +129,7 @@ function Trades() {
                 {entries?.map((entry: TradesWithImageUrl) => {
                   return (
                     <Card key={entry._id}>
-                      <CardHeader className="items-center min-h-56">
+                      <CardHeader className="items-center md:min-h-40 lg:min-h-56">
                         <Image
                           src={entry.imageUrl}
                           width="300"
